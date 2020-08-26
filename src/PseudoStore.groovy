e364dd000a08924f3cde3b6c536c791817f5044a
Khalil HIMET <khalil.himet@outlook.com>
2020-08-26 11:13:41 +0200

    Initialisation de la librairie partagée pour Jenkins

---
+++ src/PseudoStore.groovy
@@ -0,0 +1,12 @@
+class PseudoStore {
+
+    def data = [
+            'ross' : 'Rossi Oddet',
+            'gilet' : 'Emmanuel Macron',
+            'twitter' : 'Donald Trump'
+    ]
+    String findNameByPseudo(String pseudo) {
+        return data[pseudo]
+    }
+
+}
